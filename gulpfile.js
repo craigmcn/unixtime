fd6c6cd14a823355bfcf564dff69460d15f57a01
Craig McNaughton <therealcraigmcn@gmail.com>
2021-05-28 10:30:15 -0700

    Fix timezone select dropdown position
    - add safelist property to PurgeCSS
    
    Also,
    - add cache-busting to CSS and JS
      - gulp-hash, gulp-rev-rewrite

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,54 @@
 const gulp = require('gulp')
 const gulpif = require('gulp-if')
+const hash = require('gulp-hash')
+const hashOptions = { template: '<%= name %>.<%= hash %><%= ext %>' }
+const hashFilename = 'hash-manifest.json'
 const argv = require('minimist')(process.argv.slice(2))
 const env = argv.env ? argv.env : 'development'
 const output = {
-  development: './tmp',
-  production: './dist',
+    development: './tmp',
+    production: './dist',
 }
 const browserSync = require('browser-sync').create()
 
 // CSS
-gulp.task('css', (done) => {
-  const postcss = require('gulp-postcss')
-  const tailwindcss = require('tailwindcss')
-  const purgecss = require('gulp-purgecss')
-  const cleancss = require('gulp-clean-css')
-  const rename = require('gulp-rename')
+gulp.task('css', done => {
+    const postcss = require('gulp-postcss')
+    const tailwindcss = require('tailwindcss')
+    const purgecss = require('gulp-purgecss')
+    const cleancss = require('gulp-clean-css')
+    const rename = require('gulp-rename')
 
-  gulp
-    .src('./src/styles/index.css')
-    .pipe(postcss([tailwindcss('tailwind.config.js'), require('autoprefixer')]))
-    .pipe(
-      gulpif(
-        env === 'production',
-        purgecss({
-          content: ['**/*.html'],
-          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-        })
-      )
-    )
-    .pipe(gulpif(env === 'production', cleancss()))
-    .pipe(rename('./css/styles.css'))
-    .pipe(gulp.dest(output[env]))
-  done()
+    gulp.src('./src/styles/index.css')
+        .pipe(
+            postcss([
+                tailwindcss('tailwind.config.js'),
+                require('autoprefixer'),
+            ])
+        )
+        .pipe(
+            gulpif(
+                env === 'production',
+                purgecss({
+                    content: ['**/*.html'],
+                    safelist: ['autocomplete-suggestions'],
+                    defaultExtractor: content =>
+                        content.match(/[\w-/:]+(?<!:)/g) || [],
+                })
+            )
+        )
+        .pipe(gulpif(env === 'production', cleancss()))
+        .pipe(rename('./css/styles.css'))
+        .pipe(hash(hashOptions))
+        .pipe(gulp.dest(output[env]))
+        .pipe(
+            hash.manifest(hashFilename, {
+                deleteOld: true,
+                sourceDir: __dirname + output[env].substring(1),
+            })
+        )
+        .pipe(gulp.dest(output[env]))
+    done()
 })
 
 // JS
@@ -42,54 +59,68 @@ const buffer = require('vinyl-buffer')
 const sourcemaps = require('gulp-sourcemaps')
 const uglify = require('gulp-uglify')
 
-gulp.task('js', (done) => {
-  const b = browserify({
-    entries: 'src/scripts/index.js',
-    debug: env === 'production',
-  })
-
-  b.transform(
-    babelify.configure({
-      presets: ['@babel/preset-env'],
-      sourceMaps: env === 'production',
+gulp.task('js', done => {
+    const b = browserify({
+        entries: 'src/scripts/index.js',
+        debug: env === 'production',
     })
-  )
-    .bundle()
-    .pipe(source('js/scripts.js'))
-    .pipe(buffer())
-    .pipe(gulpif(env === 'production', sourcemaps.init({ loadMaps: true })))
-    .pipe(gulpif(env === 'production', uglify()))
-    .pipe(gulpif(env === 'production', sourcemaps.write('./')))
-    .pipe(gulp.dest(output[env]))
-  done()
+
+    b.transform(
+        babelify.configure({
+            presets: ['@babel/preset-env'],
+            sourceMaps: env === 'production',
+        })
+    )
+        .bundle()
+        .pipe(source('js/scripts.js'))
+        .pipe(buffer())
+        .pipe(gulpif(env === 'production', sourcemaps.init({ loadMaps: true })))
+        .pipe(gulpif(env === 'production', uglify()))
+        .pipe(gulpif(env === 'production', sourcemaps.write('./')))
+        .pipe(hash(hashOptions))
+        .pipe(gulp.dest(output[env]))
+        .pipe(
+            hash.manifest(hashFilename, {
+                deleteOld: true,
+                sourceDir: __dirname + output[env].substring(1),
+                append: true,
+            })
+        )
+        .pipe(gulp.dest(output[env]))
+    done()
 })
 
 // HTML
-gulp.task('html', (done) => {
-  gulp.src('./src/**/*.html').pipe(gulp.dest(output[env]))
-  done()
+gulp.task('html', done => {
+    const { readFileSync } = require('fs')
+    const rewrite = require('gulp-rev-rewrite')
+    const manifest = readFileSync(`${output[env]}/${hashFilename}`)
+    gulp.src('./src/**/*.html')
+        .pipe(rewrite({ manifest }))
+        .pipe(gulp.dest(output[env]))
+    done()
 })
 
 // Build
 gulp.task('build', gulp.parallel('css', 'js', 'html'))
 
 // Reload browser
-gulp.task('reload', (done) => {
-  browserSync.reload()
-  done()
+gulp.task('reload', done => {
+    browserSync.reload()
+    done()
 })
 
 // Browser sync
 gulp.task('browserSync', () => {
-  browserSync.init({
-    port: 3002,
-    server: output[env],
-    ui: false,
-  })
-  gulp.watch(
-    ['src/styles/**/*.css', 'src/scripts/**/*.js', 'src/**/*.html'],
-    gulp.series('build', 'reload')
-  )
+    browserSync.init({
+        port: 3002,
+        server: output[env],
+        ui: false,
+    })
+    gulp.watch(
+        ['src/styles/**/*.css', 'src/scripts/**/*.js', 'src/**/*.html'],
+        gulp.series('build', 'reload')
+    )
 })
 
 // Dev server
